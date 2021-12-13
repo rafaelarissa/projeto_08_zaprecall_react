1bcd5cd5e39d79b06a08df34483b0eeac2dbbd62
rafaelarissa <rafaela.larissalj@gmail.com>
2021-12-12 23:47:44 -0300

    transiçao da tela de pergunta para resposta e layout tela de resposta

--- src/Components/Flashcards.js
+++ src/Components/Flashcards.js
@@ -3,8 +3,14 @@ import "../css/style.css";
 
 import logoMini from "../assets/logo-mini.png";
 import turnIcon from "../assets/turn.png";
+import { useState } from "react";
 
 export default function Flashcards() {
+    const [front, setSide] = useState(true);
+
+    function changeSide() {
+        setSide(false);
+    }
 
     return (
         <div className="container flashcards">
@@ -12,8 +18,21 @@ export default function Flashcards() {
                 <img src={logoMini} alt="logo mini" /></div>
             <div className="card">
                 <span className="currentPosition">1/8</span>
-                <span className="title">O que é JSX?</span>
-                <img src={turnIcon} alt="turnIcon" />
+                {front ?
+                    <>
+                        <span className="title">O que é JSX?</span>
+                        <img src={turnIcon} alt="turnIcon" onClick={() => changeSide()} />
+                    </> :
+                    <>
+                        <span className="answer">Uma extensão de linguagem do JavaScript</span>
+                        <div className="buttonsFlashCard">
+                            <button>Aprendi agora</button>
+                            <button>Não lembrei</button>
+                            <button>Lembrei com esforço</button>
+                            <button>Zap!</button>
+                        </div>
+                    </>
+                }
             </div>
         </div>
     );
