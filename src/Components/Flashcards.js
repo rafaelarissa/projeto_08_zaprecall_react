146064acadc8d20cd23fd7e739cfc9a913f05107
rafaelarissa <rafaela.larissalj@gmail.com>
2021-12-13 04:07:37 -0300

    tela respostas com borda colorida

--- src/Components/Flashcards.js
+++ src/Components/Flashcards.js
@@ -7,29 +7,47 @@ import { useState } from "react";
 
 export default function Flashcards() {
     const [front, setSide] = useState(true);
+    const [answer, setAnswer] = useState(0);
+    const [questionNumber, setQuestionNumber] = useState(0);
 
-    function changeSide() {
-        setSide(false);
+    const deck = [
+        { question: "O que é JSX?", answer: "Uma extensão de linguagem do JavaScript" },
+        { question: "O React é __ ", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+        { question: "Componentes devem iniciar com __ ", answer: "Letra maiúscula" },
+        { question: "Podemos colocar __ dentro do JSX", answer: "Expressões" },
+        { question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
+        { question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
+        { question: "Usamos props para __", answer: "Passar diferentes informações para componentes " },
+        { question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+    ];
+
+    function toggleQuestion() {
+        if (answer === 0) setSide(false);
+        else setQuestionNumber(questionNumber + 1);
     }
 
     return (
         <div className="container flashcards">
             <div className="logo-mini">
                 <img src={logoMini} alt="logo mini" /></div>
-            <div className="card">
-                <span className="currentPosition">1/8</span>
+            <div className={
+                answer === 0 ? "card" : answer === 1 ? "card justLearn" :
+                    answer === 2 ? " card dontRemember" : answer === 3 ? "card almostForgot" :
+                        answer === 4 ? "card zap" : ""}>
+                <span className="currentPosition">{questionNumber + 1}/8</span>
                 {front ?
                     <>
-                        <span className="title">O que é JSX?</span>
-                        <img src={turnIcon} alt="turnIcon" onClick={() => changeSide()} />
+                        <span className="title">{deck[questionNumber].question}</span>
+                        <img src={turnIcon} alt="turnIcon" onClick={() => toggleQuestion()} />
                     </> :
                     <>
-                        <span className="answer">Uma extensão de linguagem do JavaScript</span>
+                        <span className="titleAnswer">{deck[questionNumber].question}</span>
+                        <span className="answer">{deck[questionNumber].answer}</span>
                         <div className="buttonsFlashCard">
-                            <button>Aprendi agora</button>
-                            <button>Não lembrei</button>
-                            <button>Lembrei com esforço</button>
-                            <button>Zap!</button>
+                            <button onClick={() => { setAnswer(1); setSide(true) }}>Aprendi agora</button>
+                            <button onClick={() => { setAnswer(2); setSide(true) }}>Não lembrei</button>
+                            <button onClick={() => { setAnswer(3); setSide(true) }}>Lembrei com esforço</button>
+                            <button onClick={() => { setAnswer(4); setSide(true) }}>Zap!</button>
                         </div>
                     </>
                 }
